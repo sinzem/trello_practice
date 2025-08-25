2a2b525fb5d47b1ce0f384dc308fe5b6fd979071
sinzem <sinzem@gmail.com>
2025-08-25 18:23:29 +0300

    crud_finish

---
+++ app/api/cards/[id]/route.ts
@@ -0,0 +1,70 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/app/core/prisma";
+import { updateCardDto } from "../dto";
+
+interface ICardRouteContext {
+    params: {
+        id: string;
+    }
+}
+
+export async function PUT(req: Request, { params }: ICardRouteContext) {
+    const { id } = await params;
+    const bodyRaw = await req.json();
+    const validateBody = updateCardDto.safeParse(bodyRaw);
+
+    if (!validateBody.success) {
+        return NextResponse.json(validateBody.error.issues, { status: 400 });
+    } 
+
+    const findCard = await prisma.cards.findUnique({
+        where: {
+            id,
+        },
+    });
+
+    if (!findCard) {
+        return NextResponse.json([
+            {
+                code: "not_found",
+                message: "Card not found",
+            },
+        ]);
+    } 
+
+    const card = await prisma.cards.update({
+        where: {
+            id,
+        },
+        data: validateBody.data,
+    });
+
+    return NextResponse.json(card);
+}
+
+export async function DELETE(req: Request, { params }: ICardRouteContext) {
+    const { id } = await params;
+    
+    const findCard = await prisma.cards.findUnique({
+        where: {
+            id,
+        },
+    });
+
+    if (!findCard) {
+        return NextResponse.json([
+            {
+                code: "not_found",
+                message: "Card not found",
+            },
+        ]);
+    } 
+
+    await prisma.cards.delete({
+        where: {
+            id,
+        },
+    });
+
+    return NextResponse.json({}, { status: 200 });
+}
--- app/api/cards/dto.ts
+++ app/api/cards/dto.ts
@@ -4,3 +4,16 @@ export const createCardDto = z.object({
     title: z.string().min(1),
     columnId: z.string().uuid(),
 });
+
+export const updateCardDto = createCardDto
+    .extend({
+        description: z.string().nullable(),
+    })
+    .partial();
+
+export const updateCardsOrderDto = z.array(
+    z.object({
+        id: z.string().uuid(),
+        order: z.number(),
+    })
+);
